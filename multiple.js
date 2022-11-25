11eeab406f9865a050e7ecff5e1ca8750ed36c9d
Narasimha997 <gkumar301099@gmail.com>
2022-11-25 12:50:05 +0530

    js

---
+++ multiple.js
@@ -0,0 +1,9 @@
+let a=10;
+let b=16;
+let c="Tom curise";
+let d="Mission Impossible";
+let e=true;
+let f;
+console.log(a*b);
+console.log(typeof b);
+console.log(a*c);
---
+++ new.js
@@ -0,0 +1,11 @@
+let a=10;
+let b=16;
+let c="Tom curise";
+let d="Mission Impossible";
+let e=true;
+let f;
+console.log(a+b);
+console.log(typeof a+b);
+console.log(a+d);
+console.log(typeof a+e);
+console.log(a+f);
