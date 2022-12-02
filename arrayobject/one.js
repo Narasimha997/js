5c9823a0066afba1f5a0db80d8ce1dbdab3f6858
Narasimha997 <gkumar301099@gmail.com>
2022-12-02 11:13:22 +0530

    Javascript

---
+++ arrayobject/one.js
@@ -0,0 +1,9 @@
+function grt_data(){
+    console.log("one")
+        console.log("one")
+        console.log("one")
+return "three"
+console.log("four")
+}
+let r1=grt_data();
+console.log(r1);
--- iterative/armstrong.js
+++ iterative/armstrong.js
@@ -1,7 +1,7 @@
-let a=0
-let b=371;
-let c=0;
-let d=0
+/*let a=0//rem
+let b=371;//num
+let c=0;//sum
+let d=0//temp
 c=b;
 while(c>0){
     a=c%10;
@@ -15,4 +15,20 @@ if(b==d){
 }
 else{
     console.log("Its Not An Armstrong");
+}*/
+let num=1632
+let sum=0
+let rem=0
+let temp=0
+temp=num
+while(temp>0){
+    rem=temp%10
+    temp=parseInt(temp/10)
+    sum=sum+(rem*rem*rem)
+}
+if(sum==num){
+    console.log("armstrong")
+}
+else{
+    console.log("not")
 }
