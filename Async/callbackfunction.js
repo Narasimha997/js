c64a95fad66fab8f5f15bf3a6bd3e993fc5f5925
Narasimha997 <gkumar301099@gmail.com>
2022-12-23 10:41:08 +0530

    JavaScript

---
+++ Async/callbackfunction.js
@@ -0,0 +1,23 @@
+let list=[{id:001,name:"name",sal:45000},
+{id:001,name:"name",sal:45000}]
+let create=(emp)=>{
+ setTimeout(() => {
+    list.push(emp)
+    console.log(list)
+ }, 4000);
+}
+let getemployee=()=>{
+    setTimeout(() => {
+         let rows=""
+         for(employee of list){
+            rows=rows+`<tr>
+            <td>${employee.id}</td>
+            <td>${employee.name}</td>
+            <td>${employee.sal}</td>
+            </tr>`
+         }
+         document.getElementById('def').innerHTML=rows
+    }, 4000);
+}
+create({id:003,name:"name",sal:78000});
+getemployee;
